5d745b3ee0297f326eebc88e24e6766407314727
Arsema <ama10415@nyu.edu>
2025-06-10 11:38:08 -0400

    Changes

---
+++ index.js
@@ -0,0 +1,88 @@
+const express =require('express')
+const app= express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+let notes=[
+    {
+        id:"1",
+        content:"HTML is easy",
+        important: true
+    },
+    {id: "2",
+    content: "Browser can excute only JavaScript",
+    important: false
+    },
+    {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+app.get('/',(request, response)=>{
+    response.send('<h1> Hello World! </h1>')
+})
+
+app.get('/api/notes', (request,response)=>{
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request,response)=>{
+    const id= request.params.id
+    const note= notes.find(note=>note.id === id)
+    if(note){
+        response.json(note)
+    }else{
+  response.json(404).end()
+        }
+})
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId=()=>{
+    const maxId=notes.length>0
+    ? Math.max(...notes.map(n=>Number(n.id)))
+     : 0
+    return String(maxId+1)
+}
+app.post('/api/notes', (request, response) => {
+    const body= request.body
+    if(!body.content){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const note ={
+        content:body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+    notes=notes.concat(note)
+    response.json(note)
+
+})
+const requestLogger=(request,response,next)=>{
+    console.log('Method:',request.method)
+    console.log('Path:',request.path)
+    console.log('Body:', request.body)
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
+const PORT =process.env.PORT||3003
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+// const app= http.createServer((request, response)=>{
+//     response.writeHead(200,{'Content-Type': 'application/json'})
+//     response.end(JSON.stringify(notes))
+// })
+// const PORT=3003
+// app.listen(PORT)
+// console.log(`Server running on port ${PORT}`)
